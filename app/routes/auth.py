86e786b13edbee558806bd51283efe693753b9e2
Regina Banadera <reginabanadera@gmail.com>
2026-09-24 11:22:14 +0800

    refactor: Continuation of MFA Preparation for live server

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.handlers.authLogin_handler import login, test_db_connection, protected_token, validatePass, logout, logger
+from app.handlers.authLogin_handler import login, test_db_connection, protected_token, validatePass, logout, logger, validate_MFA_token
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -7,6 +7,7 @@ auth_bp.route("/login", methods=["POST"])(login)
 auth_bp.route("/verify_token", methods=["GET"])(protected_token)
 auth_bp.route("/confirmPass", methods=["POST"])(validatePass)
 auth_bp.route("/testdb", methods=["GET"])(test_db_connection)
+auth_bp.route("/mfa-callback", methods=["POST"])(validate_MFA_token)
 
 auth_bp.route("/logout", methods=["POST"])(logout)
 auth_bp.route("/action-logger", methods=["POST"])(logger)
